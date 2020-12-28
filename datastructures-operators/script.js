e8b2b5628dc18711b6cf79f08cc7dc9571ecd05b
Jared Brown <jaredbrown1@hotmail.com>
2020-12-27 16:02:56 -0800

    destructuring objects

--- datastructures-operators/script.js
+++ datastructures-operators/script.js
@@ -25,9 +25,45 @@ const restaurant = {
       close: 24,
     },
   },
+
+  orderDelivery: function ({starterIndex = 1, mainIndex = 0, time = '20:00', address}) {
+      console.log(`Order recieved! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`)
+  }
 };
 
+restaurant.orderDelivery({
+    time: '22:30',
+    address: 'Via del sole, 21',
+    mainIndex: 2,
+    starterIndex: 2,
+})
 
+/*
+//////////////////////////////////////////////////////////
+Destructuring objects
+////////////////////////////////////////////////////////
+*/
+
+const {name, openingHours, categories} = restaurant;
+console.log(name, openingHours, categories);
+
+const {name: restaurantName, openingHours: hours, categories: tags} = restaurant;
+console.log(restaurantName, hours, tags)
+
+// default values
+const { menu = [], starterMenu: starters = []} = restaurant;
+console.log(menu, starters)
+
+// mutating variables
+let a = 111;
+let b = 999;
+const obj = {a: 23, b: 7, c: 14};
+({a, b} = obj);
+console.log(a, b);
+
+// nested objects
+const { fri: {open: o, close: c} } = openingHours
+console.log(o, c)
 
 /**
 ///////////////////////////////////////////////////////
